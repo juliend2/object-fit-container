a8f6ef369679d59e875147b9231da1fc144361b7
Julien Desrosiers <juliend2@gmail.com>
2017-10-26 14:16:34 -0400

    make it a function instead of augmenting jQuery object, which seems more complicated than I thought

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-(function ( $ ) {
+/*!
+ * objectFitContainer 1.0.0
+ *
+ * Released under the MIT license
+ * http://jquery.org/license
+ */
+var jQuery = require("jquery");
 
-  $.fn.objectFitContainer = function() {
-    return this.each(function(i, element) {
+export default (function ( $ ) {
+  "use strict";
+
+  return function(jqueryObj) {
+    return jqueryObj.each(function(i, element) {
       var naturalWidth = element.naturalWidth;
       var naturalHeight = element.naturalHeight;
       var elementHeight = $(element).height();
       var desiredElementWidth = (elementHeight * naturalWidth) / naturalHeight;
-      // console.log('naturalWidth',
-      //            naturalWidth,
-      //            'naturalHeight',
-      //            naturalHeight,
-      //            'elementHeight',
-      //            elementHeight);
       $(element).width(desiredElementWidth);
       return element;
     });
